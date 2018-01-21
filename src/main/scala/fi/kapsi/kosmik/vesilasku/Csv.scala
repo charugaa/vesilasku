e742a0eb530f2abf6446e2c7ccf9f81f34cf2732
Mikko Suniala <mikko.suniala@iki.fi>
2018-01-21 21:54:31 +0200

    Validate column names when parsing csv

--- src/main/scala/fi/kapsi/kosmik/vesilasku/Csv.scala
+++ src/main/scala/fi/kapsi/kosmik/vesilasku/Csv.scala
@@ -3,10 +3,12 @@ package fi.kapsi.kosmik.vesilasku
 import scala.io.Source
 
 object Csv {
-  def fromFile(path: String, separator: Char): Csv = {
+  def fromFile(path: String, requiredColNames: List[String], separator: Char): Csv = {
     val rawRows = Source.fromFile(path).getLines().toList
 
     val header = splitRow(rawRows.head, separator)
+    assertColNames(requiredColNames, header)
+
     val rows = rawRows.tail.map(row => splitRow(row, separator))
 
     rows.foreach(row => {
@@ -18,8 +20,18 @@ object Csv {
     new Csv(header, rows)
   }
 
-  def splitRow(row: String, separator: Char): List[String] = row.split(separator).toList
+  private def assertColNames(requiredColNames: List[String], header: List[String]): Unit = {
+    requiredColNames.foreach(colName => {
+      if (!header.contains(colName)) {
+        throw new MissingColumnException(colName)
+      }
+    })
+  }
+
+  private def splitRow(row: String, separator: Char): List[String] = row.split(separator).toList
 }
+
+class MissingColumnException(val colName: String) extends Exception
 
 class Csv(val header: List[String], val rows: List[List[String]]) {
 }
--- src/test/scala/fi/kapsi/kosmik/vesilasku/VesilaskuSpec.scala
+++ src/test/scala/fi/kapsi/kosmik/vesilasku/VesilaskuSpec.scala
@@ -3,9 +3,21 @@ package fi.kapsi.kosmik.vesilasku
 import org.scalatest.{FlatSpec, Matchers}
 
 class VesilaskuSpec extends FlatSpec with Matchers {
-  "The Csv object" should "read csv from file" in {
-    val csv = Csv.fromFile(getClass.getResource("data-sample.csv").getPath, '\t')
-    csv.header shouldEqual List("Col1", "Col2", "Col3")
+  behavior of "The Csv object"
+
+  it should "fail if required col names are missing" in {
+    val colNames = List("Col1", "Col4")
+    assertThrows[MissingColumnException] {
+      Csv.fromFile(getClass.getResource("data-sample.csv").getPath, colNames, '\t')
+    }
+  }
+
+  it should "read csv from file" in {
+    val colNames = List("Col1", "Col2", "Col3")
+
+    val csv = Csv.fromFile(getClass.getResource("data-sample.csv").getPath, colNames, '\t')
+
+    csv.header shouldEqual colNames
     csv.rows shouldEqual List(
       List("v11", "v12", "v13"),
       List("v21", "v22", "v23")
