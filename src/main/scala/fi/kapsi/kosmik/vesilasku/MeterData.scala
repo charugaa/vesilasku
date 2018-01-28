dfd7c849d3f36cdf698a2b600c2d3cdf1c4129d3
Mikko Suniala <mikko.suniala@iki.fi>
2018-01-28 18:52:10 +0200

    Refactor: move internal types in MeterData inside companion object

--- src/main/scala/fi/kapsi/kosmik/vesilasku/MeterData.scala
+++ src/main/scala/fi/kapsi/kosmik/vesilasku/MeterData.scala
@@ -18,39 +18,39 @@ object MeterData {
     def readoutDate(): LocalDate = Parser.parseDate(csvRow.col(Columns.readoutDate))
   }
 
-  val colNames: List[String] = List(Columns.identificationNumber, Columns.readoutDate) ++
-    (1 to 15).map(i => Columns.monthlyVolume(i)).toList
-
   def fromFile(path: String): MeterData = new MeterData(fromCsvFile(path, colNames, '\t'))
+
+  /**
+    * Functions for parsing values using meter data specific formatting.
+    */
+  private object Parser {
+    /** DateTimeFormatter is thread safe. */
+    private val dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+
+    def parseDate(formatted: String): _root_.java.time.LocalDate = LocalDate.parse(formatted, dateFormatter)
+
+    def parseDouble(formatted: String): Double = formatted.replace(",", ".").toDouble
+  }
+
+  private object Columns {
+    private val monthRange: Int = 15
+
+    def identificationNumber = "Identification number"
+
+    def monthlyVolume(monthsFromReadout: Int): String = {
+      if (monthsFromReadout > monthRange) {
+        throw new IllegalArgumentException(s"Month $monthsFromReadout is out of range")
+      }
+      s"Monthly volume ${-monthsFromReadout}"
+    }
+
+    def readoutDate = "Readout date"
+  }
+
+  private val colNames: List[String] = List(Columns.identificationNumber, Columns.readoutDate) ++
+    (1 to 15).map(i => Columns.monthlyVolume(i)).toList
 }
 
 class MeterData(private val csv: Csv) {
   def rows(): Stream[Row] = csv.rows().map(csvRow => new Row(csvRow))
-}
-
-/**
-  * Functions for parsing values using meter data specific formatting.
-  */
-private object Parser {
-  /** DateTimeFormatter is thread safe. */
-  private val dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
-
-  def parseDate(formatted: String): _root_.java.time.LocalDate = LocalDate.parse(formatted, dateFormatter)
-
-  def parseDouble(formatted: String): Double = formatted.replace(",", ".").toDouble
-}
-
-private object Columns {
-  private val monthRange: Int = 15
-
-  def identificationNumber = "Identification number"
-
-  def monthlyVolume(monthsFromReadout: Int): String = {
-    if (monthsFromReadout > monthRange) {
-      throw new IllegalArgumentException(s"Month $monthsFromReadout is out of range")
-    }
-    s"Monthly volume ${-monthsFromReadout}"
-  }
-
-  def readoutDate = "Readout date"
 }
